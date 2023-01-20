0a4c5842e040ee6a3e9593f69b0275c8405ae5eb
Hugge25 <hugo.paulsson@hotmail.com>
2023-01-20 10:55:54 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Brogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplikatorn 3000. Skriv nummer separerade med * för att multiplicera");
+            string[] sträng = Console.ReadLine().Split("*");
+            Int64[] nummer = Array.ConvertAll(sträng, s => Int64.Parse(s)); // Använder Int64 så att man kan multiplicera större tal
+            Int64 x = 1;
+            foreach (Int64 i in nummer)
+            {   x = x * i;  }
+            Console.WriteLine(x);
+            
+        }
+    }
+}
